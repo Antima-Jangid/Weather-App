183ff8a8585faa6bfb1501631ad32a796600f012
Antima Jangid <jangidantima270@gmail.com>
2026-04-03 08:17:59 +0530

    Added weather API integration

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,9 +5,10 @@ import { useState } from "react";
 export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
-  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "ab852ae814ab3a664e020d7bed0024ce";
-
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+console.log(API_URL);
+console.log(API_KEY);
   let getWeatherInfo = async () => {
     try {
       let response = await fetch(
